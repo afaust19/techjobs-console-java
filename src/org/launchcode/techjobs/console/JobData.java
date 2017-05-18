5cb21bcc3030f2a6b93568efe292d1f4776544bc
AnnMarie Faust <amn5336@gmail.com>
2017-05-17 20:45:38 -0500

    Finished TechJobs (Console Edition)

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,9 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static java.lang.String.CASE_INSENSITIVE_ORDER;
 
 /**
  * Created by LaunchCode
@@ -19,6 +22,7 @@ public class JobData {
     private static final String DATA_FILE = "resources/job_data.csv";
     private static Boolean isDataLoaded = false;
 
+    //make allJobs list case insensitive (CASE_INSENSITIVE_ORDER)
     private static ArrayList<HashMap<String, String>> allJobs;
 
     /**
@@ -57,33 +61,72 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
-    public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
+    public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) { //*******
 
         // load data, if not already loaded
-        loadData();
+        loadData();  //*******
 
-        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>(); //*******
 
-        for (HashMap<String, String> row : allJobs) {
+        for (HashMap<String, String> row : allJobs) {  //*******
 
-            String aValue = row.get(column);
+            String aValue = row.get(column);  //******* //gets the column the user want to search in
 
-            if (aValue.contains(value)) {
-                jobs.add(row);
+
+            if (aValue.toUpperCase().contains(value.toUpperCase())) {
+            //if (aValue.contains(value)) {  //*******   //does each entry in that column contain the search term?
+                jobs.add(row);  //*******
             }
         }
 
-        return jobs;
+
+        return jobs; //*******
     }
 
+    // make searchTerm case insensitive (String.CASE_INSENSITIVE_ORDER)
+
+    public static ArrayList<HashMap<String, String>> searchAllColumns(String searchTerm) { //*******
+
+        // load data, if not already loaded
+        loadData();  //*******
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>(); //*******
+
+        // each row (job entry) in allJobs is a HashMap
+        // iterate through each row in the ArrayList to find each HashMap that contains the searchTerm
+
+        for (HashMap<String, String> row : allJobs) { //*******
+
+            // if the row contains the searchTerm (HashMap value), add the row to jobs
+            // searchTerm needs to match exact value of HashMap, not just one word
+
+            //if (row.containsValue(searchTerm)) {  //*******
+            // equalsIgnoreCase for both searchTerm and each value in row
+
+            for (Map.Entry<String, String> entry : row.entrySet()) {
+                String value = entry.getValue();
+
+                if (value.toUpperCase().contains(searchTerm.toUpperCase())) {
+                    jobs.add(row);   //*******
+                    //add a break to end your search (you already found an entry with the search term, you don't
+                    //want to keep looking for the same term in the same HashMap otherwise you'll return the same
+                    //HashMap twice.
+                    break;
+
+                }
+            }
+
+        }
+        return jobs;   //*******
+    }
     /**
      * Read in data from a CSV file and store it in a list
      */
@@ -125,4 +168,5 @@ public class JobData {
         }
     }
 
-}
+    }
+
